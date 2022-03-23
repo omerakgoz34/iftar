e7820e021651d1538c5b3da2126d02234ca2e212
omerakgoz34 <omerakgoz34@gmail.com>
2022-03-24 00:51:03 +0300

    FLTK ile yeni arayüz (bitmedi)

--- README.md
+++ README.md
@@ -1,24 +1,22 @@
 # iftar
 
-**İftar vaktini öğrenebileceğiniz küçük bir komut satırı programı**
+**İftar vaktini öğrenebileceğiniz küçük bir uygulama**
 
 ## İndirme Linkleri:
-[Windows amd64 v0.1.2](https://github.com/omerakgoz34/iftar/releases/download/v0.1.2/iftar_v0.1.2_windows_amd64.zip) ([Scoop](https://github.com/omerakgoz34/iftar/raw/f8e14390d2d17a9dcf76366d10ed4a8abfade9be/iftar.json))  
-[Linux amd64 v0.1.2](https://github.com/omerakgoz34/iftar/releases/download/v0.1.2/iftar_v0.1.2_linux_amd64.zip)  
-[macOS amd64 v0.1.2](https://github.com/omerakgoz34/iftar/releases/download/v0.1.2/iftar_v0.1.2_macos_amd64.zip)  
-[Android arm64 v0.1.2](https://github.com/omerakgoz34/iftar/releases/download/v0.1.2/iftar_v0.1.2_android_arm64.zip)
+[Windows amd64 v0.2.0](https://github.com/omerakgoz34/iftar/releases/download/v0.2.0/iftar_v0.2.0_windows_amd64.zip) ([Scoop](https://github.com/omerakgoz34/iftar/raw/main/iftar.json))  
+[Linux amd64 v0.2.0](https://github.com/omerakgoz34/iftar/releases/download/v0.2.0/iftar_v0.2.0_linux_amd64.zip)  
+[macOS amd64 v0.2.0](https://github.com/omerakgoz34/iftar/releases/download/v0.2.0/iftar_v0.2.0_macos_amd64.zip)  
+[Android arm64 v0.1.2](https://github.com/omerakgoz34/iftar/releases/download/v0.1.2/iftar_v0.2.0_android_arm64.zip)
 
 ## Özellikler
 
-* İftar vaktini ve kalan süreyi gösterir
+* Sistem satini ve iftar vaktini gösterir
 * Vakit bilgileri her gece saat 00:00'dan sonra yenilenir
 * Rust diliyle yazılmıştır ❤ 🦀
-* İftar saat bilgilerini Diyanet İşleri Bakanlığı'nın [sitesinden](https://namazvakitleri.diyanet.gov.tr/tr-TR) alır (siteden kaynaklı olarak bazen hata verebilir)
-* HTTPS bağlantısı kullanır (rustls)
+* İftar saat bilgilerini Diyanet İşleri Bakanlığı'nın [sitesinden](https://namazvakitleri.diyanet.gov.tr/tr-TR) alır (siteden kaynaklı olarak bazen donup kalabilir)
 
 ## Ekran Görüntüleri
-![Screenshot_209](https://github.com/omerakgoz34/iftar/raw/3bcbd02b0e79f18b3eb81dc1ec25daef2f701899/screenshots/ss1.png)  
-![Screenshot_210](https://github.com/omerakgoz34/iftar/raw/3bcbd02b0e79f18b3eb81dc1ec25daef2f701899/screenshots/ss2.png)
-
-## Termux Örneği
+![ss1](https://github.com/omerakgoz34/iftar/raw/main/screenshots/ss1.png)  
+  
+## Android Termux Örneği
 https://youtu.be/tT9vjz2a9tM
--- src/main.rs
+++ src/main.rs
@@ -1,176 +1,220 @@
+// Disable windows console on release builds
+#![cfg_attr(windows, cfg_attr(not(debug_assertions), windows_subsystem = "windows"))]
+
+use fltk::prelude::*;
 use chrono::prelude::*;
+use indexmap::indexmap;
+
+const APP_NAME: &str = "İftar";
+const APP_VERSION: &str = env!("CARGO_PKG_VERSION");
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-
-    // Hakkında
-    if args.len() < 2 {
-        println!("");
-        println!("* iftar v{} by omerakgoz34", env!("CARGO_PKG_VERSION"));
-        println!("* Kullanım: iftar <şehir>");
-        println!("* NOT: İftar vakti her gece 00:00'dan sonra yenilenir.");
-        println!("* Hata bildirimi ve sorular için: https://github.com/omerakgoz34/iftar/issues/new");
-        return
-    }
-
-    let url_diyanet = "https://namazvakitleri.diyanet.gov.tr/tr-TR/";
-
-    // Şehirler
-    let mut cities = std::collections::HashMap::new();
-    cities.insert("istanbul", "9541/istanbul-icin-namaz-vakti");
-    cities.insert("tekirdağ", "9879/tekirdag-icin-namaz-vakti");
-    cities.insert("ordu", "9782/ordu-icin-namaz-vakti");
-    cities.insert("ünye", "9783/unye-icin-namaz-vakti");
-    cities.insert("ikizce", "9777/ikizce-icin-namaz-vakti");
-    cities.insert("adana", "9146/adana-icin-namaz-vakti");
-    cities.insert("adıyaman", "9158/adiyaman-icin-namaz-vakti");
-    cities.insert("afyonkarahisar", "9167/afyonkarahisar-icin-namaz-vakti");
-    cities.insert("ağrı", "9185/agri-icin-namaz-vakti");
-    cities.insert("aksaray", "9193/aksaray-icin-namaz-vakti");
-    cities.insert("amasya", "9198/amasya-icin-namaz-vakti");
-    cities.insert("ankara", "9206/ankara-icin-namaz-vakti");
-    cities.insert("antalya", "9225/antalya-icin-namaz-vakti");
-    cities.insert("ardahan", "9238/ardahan-icin-namaz-vakti");
-    cities.insert("artvin", "9246/artvin-icin-namaz-vakti");
-    cities.insert("aydın", "9252/aydin-icin-namaz-vakti");
-    cities.insert("balıkesir", "9270/balikesir-icin-namaz-vakti");
-    cities.insert("bartın", "9285/bartin-icin-namaz-vakti");
-    cities.insert("batman", "9288/batman-icin-namaz-vakti");
-    cities.insert("bayburt", "9295/bayburt-icin-namaz-vakti");
-    cities.insert("bilecik", "9297/bilecik-icin-namaz-vakti");
-    cities.insert("bingöl", "9303/bingol-icin-namaz-vakti");
-    cities.insert("bitlis", "9311/bitlis-icin-namaz-vakti");
-    cities.insert("bolu", "9315/bolu-icin-namaz-vakti");
-    cities.insert("burdur", "9327/burdur-icin-namaz-vakti");
-    cities.insert("bursa", "9335/bursa-icin-namaz-vakti");
-    cities.insert("çanakkale", "9352/canakkale-icin-namaz-vakti");
-    cities.insert("çankırı", "9359/cankiri-icin-namaz-vakti");
-    cities.insert("çorum", "9370/corum-icin-namaz-vakti");
-    cities.insert("denizli", "9392/denizli-icin-namaz-vakti");
-    cities.insert("diyarbakır", "9402/diyarbakir-icin-namaz-vakti");
-    cities.insert("düzce", "9414/duzce-icin-namaz-vakti");
-    cities.insert("edirne", "9419/edirne-icin-namaz-vakti");
-    cities.insert("elazığ", "9432/elazig-icin-namaz-vakti");
-    cities.insert("erzincan", "9440/erzincan-icin-namaz-vakti");
-    cities.insert("erzurum", "9451/erzurum-icin-namaz-vakti");
-    cities.insert("eskişehir", "9470/eskisehir-icin-namaz-vakti");
-    cities.insert("gaziantep", "9479/gaziantep-icin-namaz-vakti");
-    cities.insert("giresun", "9494/giresun-icin-namaz-vakti");
-    cities.insert("gümüşhane", "9501/gumushane-icin-namaz-vakti");
-    cities.insert("hakkari", "9507/hakkari-icin-namaz-vakti");
-    cities.insert("hatay", "20089/hatay-icin-namaz-vakti");
-    cities.insert("ığdır", "9522/igdir-icin-namaz-vakti");
-    cities.insert("ısparta", "9528/isparta-icin-namaz-vakti");
-    cities.insert("izmir", "9560/izmir-icin-namaz-vakti");
-    cities.insert("kahramanmaraş", "9577/kahramanmaras-icin-namaz-vakti");
-    cities.insert("karabük", "9581/karabuk-icin-namaz-vakti");
-    cities.insert("karaman", "9587/karaman-icin-namaz-vakti");
-    cities.insert("kars", "9594/kars-icin-namaz-vakti");
-    cities.insert("kastamonu", "9609/kastamonu-icin-namaz-vakti");
-    cities.insert("kayseri", "9620/kayseri-icin-namaz-vakti");
-    cities.insert("kilis", "9629/kilis-icin-namaz-vakti");
-    cities.insert("kırıkkale", "9635/kirikkale-icin-namaz-vakti");
-    cities.insert("kırklareli", "9638/kirklareli-icin-namaz-vakti");
-    cities.insert("kırşehir", "9646/kirsehir-icin-namaz-vakti");
-    cities.insert("kocaeli", "9654/kocaeli-icin-namaz-vakti");
-    cities.insert("konya", "9676/konya-icin-namaz-vakti");
-    cities.insert("kütahya", "9689/kutahya-icin-namaz-vakti");
-    cities.insert("malatya", "9703/malatya-icin-namaz-vakti");
-    cities.insert("manisa", "9716/manisa-icin-namaz-vakti");
-    cities.insert("mardin", "9726/mardin-icin-namaz-vakti");
-    cities.insert("mersin", "9737/mersin-icin-namaz-vakti");
-    cities.insert("muğla", "9747/mugla-icin-namaz-vakti");
-    cities.insert("muş", "9755/mus-icin-namaz-vakti");
-    cities.insert("nevşehir", "9760/nevsehir-icin-namaz-vakti");
-    cities.insert("niğde", "9766/nigde-icin-namaz-vakti");
-    cities.insert("osmaniye", "9788/osmaniye-icin-namaz-vakti");
-    cities.insert("rize", "9799/rize-icin-namaz-vakti");
-    cities.insert("sakarya", "9807/sakarya-icin-namaz-vakti");
-    cities.insert("samsun", "9819/samsun-icin-namaz-vakti");
-    cities.insert("şanlıurfa", "9831/sanliurfa-icin-namaz-vakti");
-    cities.insert("siirt", "9839/siirt-icin-namaz-vakti");
-    cities.insert("sinop", "9847/sinop-icin-namaz-vakti");
-    cities.insert("şırnak", "9854/sirnak-icin-namaz-vakti");
-    cities.insert("sivas", "9868/sivas-icin-namaz-vakti");
-    cities.insert("tekirdağ", "9879/tekirdag-icin-namaz-vakti");
-    cities.insert("tokat", "9887/tokat-icin-namaz-vakti");
-    cities.insert("trabzon", "9905/trabzon-icin-namaz-vakti");
-    cities.insert("tunceli", "9914/tunceli-icin-namaz-vakti");
-    cities.insert("uşak", "9919/usak-icin-namaz-vakti");
-    cities.insert("van", "9930/van-icin-namaz-vakti");
-    cities.insert("yalova", "9935/yalova-icin-namaz-vakti");
-    cities.insert("yozgat", "9949/yozgat-icin-namaz-vakti");
-    cities.insert("zonguldak", "9955/zonguldak-icin-namaz-vakti");
-    
-    // Şehir kodu
-    let city = args[1].to_owned();
-    let city_code = match cities.get(&city[..]) {
-        Some(&res) => res,
-        _ => {
-            println!("");
-            println!(">>> HATA: Girdiğiniz şehir bulunamadı! İsimleri bitişik ve küçük harflerle yazınız.");
-            return;
-        },
-    };
-
-    // Final URL
-    let url = &(url_diyanet.to_owned() + city_code)[..];
-    let response = match  minreq::get(url).send() {
-        Ok(res) => res,
-        Err(_) => {
-            println!("");
-            println!(">>> HATA: Sunucuya bağlanılamadı! İnternete bağlı olduğunuzdan emin olunuz.");
-            return;
-        },
-    };
-
-    // Sunucudan gelen yanıtı yazıya çevirme
-    let response = match response.as_str() {
-        Ok(res) => res,
-        Err(_) => {
-            println!("");
-            println!(">>> HATA: Sunucudan alınan veri okunamadı! (UTF-8 hatası)");
-            return;
-        },
-    };
-
-    // CSS selector kullanarak iftar saati bilgisini bulma
-    let fragment = scraper::Html::parse_fragment(response);
-    let selector = match scraper::Selector::parse("div.tpt-cell[data-vakit-name='aksam'] .tpt-time") {
-        Ok(res) => res,
-        Err(_) => {
-            println!("");
-            println!(">>> HATA: İftar saati bulunamadı! Tekrar deneyiniz.");
-            return;
-        },
-    };
-    let iftar = fragment.select(&selector).next().unwrap().inner_html();
-
-    // Zaman hesaplamaları
-    let now = Local::now();
-    let iftar_split: Vec<&str> = iftar.split(":").collect();
-    let iftar_time = Local.ymd(now.year(), now.month(), now.day()).and_hms(iftar_split[0].parse::<u32>().unwrap(), iftar_split[1].parse::<u32>().unwrap(), 0);
-    
     #[cfg(debug_assertions)]
-    {
-        println!("iftar_time: {}", iftar_time);
-        println!("now: {}", now);
-        println!("kalan: {}", Local.timestamp(iftar_time.timestamp() - now.timestamp(), 0));
-        println!("geçen: {}", Local.timestamp(now.timestamp() - iftar_time.timestamp(), 0));
+    println!("DEBUG >>> App: {} v{}", APP_NAME, APP_VERSION);
+
+    // İller
+    let cities = indexmap!{
+        "Adana" => "9146/adana-icin-namaz-vakti",
+        "Adıyaman" => "9158/adiyaman-icin-namaz-vakti",
+        "Afyonkarahisar" => "9167/afyonkarahisar-icin-namaz-vakti",
+        "Ağrı" => "9185/agri-icin-namaz-vakti",
+        "Aksaray" => "9193/aksaray-icin-namaz-vakti",
+        "Amasya" => "9198/amasya-icin-namaz-vakti",
+        "Ankara" => "9206/ankara-icin-namaz-vakti",
+        "Antalya" => "9225/antalya-icin-namaz-vakti",
+        "Ardahan" => "9238/ardahan-icin-namaz-vakti",
+        "Artvin" => "9246/artvin-icin-namaz-vakti",
+        "Aydın" => "9252/aydin-icin-namaz-vakti",
+        "Balıkesir" => "9270/balikesir-icin-namaz-vakti",
+        "Bartın" => "9285/bartin-icin-namaz-vakti",
+        "Batman" => "9288/batman-icin-namaz-vakti",
+        "Bayburt" => "9295/bayburt-icin-namaz-vakti",
+        "Bilecik" => "9297/bilecik-icin-namaz-vakti",
+        "Bingöl" => "9303/bingol-icin-namaz-vakti",
+        "Bitlis" => "9311/bitlis-icin-namaz-vakti",
+        "Bolu" => "9315/bolu-icin-namaz-vakti",
+        "Burdur" => "9327/burdur-icin-namaz-vakti",
+        "Bursa" => "9335/bursa-icin-namaz-vakti",
+        "Çanakkale" => "9352/canakkale-icin-namaz-vakti",
+        "Çankırı" => "9359/cankiri-icin-namaz-vakti",
+        "Çorum" => "9370/corum-icin-namaz-vakti",
+        "Denizli" => "9392/denizli-icin-namaz-vakti",
+        "Diyarbakır" => "9402/diyarbakir-icin-namaz-vakti",
+        "Düzce" => "9414/duzce-icin-namaz-vakti",
+        "Edirne" => "9419/edirne-icin-namaz-vakti",
+        "Elazığ" => "9432/elazig-icin-namaz-vakti",
+        "Erzincan" => "9440/erzincan-icin-namaz-vakti",
+        "Erzurum" => "9451/erzurum-icin-namaz-vakti",
+        "Eskişehir" => "9470/eskisehir-icin-namaz-vakti",
+        "Gaziantep" => "9479/gaziantep-icin-namaz-vakti",
+        "Giresun" => "9494/giresun-icin-namaz-vakti",
+        "Gümüşhane" => "9501/gumushane-icin-namaz-vakti",
+        "Hakkari" => "9507/hakkari-icin-namaz-vakti",
+        "Hatay" => "20089/hatay-icin-namaz-vakti",
+        "Iğdır" => "9522/igdir-icin-namaz-vakti",
+        "Isparta" => "9528/isparta-icin-namaz-vakti",
+        "İstanbul" => "9541/istanbul-icin-namaz-vakti",
+        "İzmir" => "9560/izmir-icin-namaz-vakti",
+        "Kahramanmaraş" => "9577/kahramanmaras-icin-namaz-vakti",
+        "Karabük" => "9581/karabuk-icin-namaz-vakti",
+        "Karaman" => "9587/karaman-icin-namaz-vakti",
+        "Kars" => "9594/kars-icin-namaz-vakti",
+        "Kastamonu" => "9609/kastamonu-icin-namaz-vakti",
+        "Kayseri" => "9620/kayseri-icin-namaz-vakti",
+        "Kilis" => "9629/kilis-icin-namaz-vakti",
+        "kırıkkale" => "9635/kirikkale-icin-namaz-vakti",
+        "Kırklareli" => "9638/kirklareli-icin-namaz-vakti",
+        "Kırşehir" => "9646/kirsehir-icin-namaz-vakti",
+        "Kocaeli" => "9654/kocaeli-icin-namaz-vakti",
+        "Konya" => "9676/konya-icin-namaz-vakti",
+        "Kütahya" => "9689/kutahya-icin-namaz-vakti",
+        "Malatya" => "9703/malatya-icin-namaz-vakti",
+        "Manisa" => "9716/manisa-icin-namaz-vakti",
+        "Mardin" => "9726/mardin-icin-namaz-vakti",
+        "Mersin" => "9737/mersin-icin-namaz-vakti",
+        "Muğla" => "9747/mugla-icin-namaz-vakti",
+        "Muş" => "9755/mus-icin-namaz-vakti",
+        "Nevşehir" => "9760/nevsehir-icin-namaz-vakti",
+        "Niğde" => "9766/nigde-icin-namaz-vakti",
+        "Ordu" => "9782/ordu-icin-namaz-vakti",
+        "Osmaniye" => "9788/osmaniye-icin-namaz-vakti",
+        "Rize" => "9799/rize-icin-namaz-vakti",
+        "Sakarya" => "9807/sakarya-icin-namaz-vakti",
+        "Samsun" => "9819/samsun-icin-namaz-vakti",
+        "Şanlıurfa" => "9831/sanliurfa-icin-namaz-vakti",
+        "Siirt" => "9839/siirt-icin-namaz-vakti",
+        "Sinop" => "9847/sinop-icin-namaz-vakti",
+        "Şırnak" => "9854/sirnak-icin-namaz-vakti",
+        "Sivas" => "9868/sivas-icin-namaz-vakti",
+        "Tekirdağ" => "9879/tekirdag-icin-namaz-vakti",
+        "Tokat" => "9887/tokat-icin-namaz-vakti",
+        "Trabzon" => "9905/trabzon-icin-namaz-vakti",
+        "Tunceli" => "9914/tunceli-icin-namaz-vakti",
+        "Uşak" => "9919/usak-icin-namaz-vakti",
+        "Van" => "9930/van-icin-namaz-vakti",
+        "Yalova" => "9935/yalova-icin-namaz-vakti",
+        "Yozgat" => "9949/yozgat-icin-namaz-vakti",
+        "Zonguldak" => "9955/zonguldak-icin-namaz-vakti",
+    };
+
+    // FLTK app
+    let app = fltk::app::App::default().with_scheme(fltk::app::Scheme::Gtk);
+    fltk::app::set_visible_focus(false);
+
+    // FLTK window
+    let mut ui_win = fltk::window::Window::default().with_size(270, 140).center_screen().with_label(&(APP_NAME.to_owned() + " v" + APP_VERSION)).center_screen();
+    ui_win.make_resizable(false);
+    // ui_win.set_icon(Some(fltk::image::PngImage::from_data(include_bytes!("../icon.png")).unwrap()));
+
+    // UI List Cities 
+    let mut ui_list_cities = fltk::misc::InputChoice::default().with_size(250, 30).with_pos(10, 10).with_label("");
+    ui_list_cities.set_tooltip("Konum Seçiniz");
+    ui_list_cities.set_value("Konum Seçiniz");
+    for (city, _) in cities.iter() {
+        ui_list_cities.add(city);
     }
 
-    // İftara daha vakit var
-    if now < iftar_time {
-        let remaining_time = Local.timestamp(iftar_time.timestamp() - now.timestamp(), 0);
-        println!("");
-        println!("* Şehir: {}", city);
-        println!("* İftar vakti: {}", iftar);
-        println!("* Kalan süre: {} saat {} dakika", remaining_time.hour() - 2, remaining_time.minute() + 1);
-    }
-    // İftar geçti
-    if now > iftar_time {
-        println!("");
-        println!("* Şehir: {}", city);
-        println!("* İftar vakti: {} (geçti)", iftar);
-    }
+    // UI Time
+    let mut ui_time = fltk::text::TextDisplay::default().with_size(100, 30).with_pos(10, 100).with_label("Saat");
+    ui_time.set_buffer(fltk::text::TextBuffer::default());
+    ui_time.set_text_size(20);
+    fltk::text::TextBuffer::set_text(&mut ui_time.buffer().unwrap(), "     .....");
+    let mut ui_time_iftar = fltk::text::TextDisplay::default().with_size(100, 30).with_pos(160, 100).with_label("İftar");
+    ui_time_iftar.set_buffer(fltk::text::TextBuffer::default());
+    ui_time_iftar.set_text_size(20);
+    fltk::text::TextBuffer::set_text(&mut ui_time_iftar.buffer().unwrap(), "     .....   ");
+
+    // let input = fltk::input::Input::default().with_size(250, 30).with_pos(110, 10).with_label("Şehrin URL'si:");
+
+    let mut ui_button_fetch = fltk::button::Button::default().with_size(75, 30).with_pos(98, 55).with_label("Kontrol Et");
+    ui_button_fetch.set_callback({
+        let mut wn = ui_win.clone();
+        move |_| {
+            wn.set_cursor(fltk::enums::Cursor::Wait);
+            let location_input = ui_list_cities.value().unwrap();
+
+            #[cfg(debug_assertions)]
+            println!("DEBUG >>> Girilen şehir: {}", location_input);
+    
+            let url_diyanet = "https://namazvakitleri.diyanet.gov.tr/tr-TR/";
+    
+            // Şehir kodu
+            let location_url = match cities.get(&location_input[..]) {
+                Some(&res) => res,
+                _ => {
+                    wn.set_cursor(fltk::enums::Cursor::Default);
+                    #[cfg(debug_assertions)]
+                    println!("DEBUG >>> HATA: Girdiğiniz şehir bulunamadı! İsimleri bitişik ve küçük harflerle yazınız.");
+                    return;
+                },
+            };
+    
+            // Final URL
+            let url = &(url_diyanet.to_owned() + location_url)[..];
+            let response = match  minreq::get(url).send() {
+                Ok(res) => res,
+                Err(_) => {
+                    wn.set_cursor(fltk::enums::Cursor::Default);
+                    #[cfg(debug_assertions)]
+                    println!("DEBUG >>> HATA: Sunucuya bağlanılamadı! İnternete bağlı olduğunuzdan emin olunuz.");
+                    return;
+                },
+            };
+    
+            // Sunucudan gelen yanıtı yazıya çevirme
+            let response = match response.as_str() {
+                Ok(res) => res,
+                Err(_) => {
+                    wn.set_cursor(fltk::enums::Cursor::Default);
+                    #[cfg(debug_assertions)]
+                    println!("DEBUG >>> HATA: Sunucudan alınan veri okunamadı! (UTF-8 hatası)");
+                    return;
+                },
+            };
+    
+            // CSS selector kullanarak iftar saati bilgisini bulma
+            let fragment = scraper::Html::parse_fragment(response);
+            let selector = match scraper::Selector::parse("div.tpt-cell[data-vakit-name='aksam'] .tpt-time") {
+                Ok(res) => res,
+                Err(_) => {
+                    wn.set_cursor(fltk::enums::Cursor::Default);
+                    #[cfg(debug_assertions)]
+                    println!("DEBUG >>> HATA: İftar saati bulunamadı! Tekrar deneyiniz.");
+                    return;
+                },
+            };
+            let iftar = fragment.select(&selector).next().unwrap().inner_html();
+            #[cfg(debug_assertions)]
+            println!("DEBUG >>> iftar: {}", iftar);
+    
+            // Zaman hesaplamaları
+            let now = Local::now();
+            #[cfg(debug_assertions)]
+            let iftar_split: Vec<&str> = iftar.split(":").collect();
+            #[cfg(debug_assertions)]
+            let iftar_time = Local.ymd(now.year(), now.month(), now.day()).and_hms(iftar_split[0].parse::<u32>().unwrap(), iftar_split[1].parse::<u32>().unwrap(), 0);
+    
+            #[cfg(debug_assertions)]
+            {
+                println!("DEBUG >>> iftar_timestamp: {}", iftar_time);
+                println!("DEBUG >>> now_timestamp: {}", now);
+                println!("DEBUG >>> kalan_timestamp: {}", Local.timestamp(iftar_time.timestamp() - now.timestamp(), 0));
+                println!("DEBUG >>> geçen_timestamp: {}", Local.timestamp(now.timestamp() - iftar_time.timestamp(), 0));
+            }
+    
+            fltk::text::TextBuffer::set_text(&mut ui_time.buffer().unwrap(), &("   ".to_owned() + &now.format("%H:%M").to_string()[..])[..]);
+            fltk::text::TextBuffer::set_text(&mut ui_time_iftar.buffer().unwrap(), &("   ".to_string() + &iftar[..])[..]);
+            #[cfg(debug_assertions)]
+            {
+                println!("DEBUG >>> Saat: {}", now.format("%H:%M"));
+                println!("DEBUG >>> İftar vakti: {}", iftar);
+            }
+
+            wn.set_cursor(fltk::enums::Cursor::Default);
+        }
+    });
+
+    ui_win.end();
+    ui_win.show();
+    app.run().unwrap();
 }
